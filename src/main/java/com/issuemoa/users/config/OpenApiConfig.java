e94a33cd8f566f1a7796575d1c8f91d074ad9498
venh <conf312@naver.com>
2024-01-07 18:14:23 +0900

    [Modify] OpenAPI 접근 URL 변경

--- src/main/java/com/issuemoa/users/config/OpenApiConfig.java
+++ src/main/java/com/issuemoa/users/config/OpenApiConfig.java
@@ -1,16 +1,15 @@
 package com.issuemoa.users.config;
 
-import org.springdoc.core.GroupedOpenApi;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
     @Bean
-    public GroupedOpenApi api() {
-        return GroupedOpenApi.builder()
-                .group("api")
-                .pathsToMatch("/**")
-                .build();
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"));
     }
 }
